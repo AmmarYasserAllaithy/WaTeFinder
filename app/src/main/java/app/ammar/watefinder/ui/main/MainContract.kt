c6416e3f7fdfb3a0e912df5a35ec3d219071077b
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-04 01:16:21 +0300

    ui: add main contract

---
+++ app/src/main/java/app/ammar/watefinder/ui/main/MainContract.kt
@@ -0,0 +1,21 @@
+package app.ammar.watefinder.ui.main
+
+import androidx.annotation.StringRes
+import app.ammar.watefinder.domain.model.AccountModel
+
+
+object MainContract {
+
+    data class State(
+        val number: String = "",
+        val message: String = "",
+        val history: List<AccountModel> = emptyList(),
+        val isNumberError: Boolean = false,
+    )
+
+    sealed interface Effect {
+        data class OpenUrl(val url: String) : Effect
+        data class ShowToast(@StringRes val messageResId: Int) : Effect
+    }
+
+}
