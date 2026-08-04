4d9d17604715d5bed684a0b3b7f3833d527bd474
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-04 03:52:04 +0300

    add ui theme and colors

---
+++ app/src/main/java/app/ammar/watefinder/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+package app.ammar.watefinder.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+// BlueSlate Palette - Light
+val BlueSlatePrimary = Color(0xFF3B7080)
+val BlueSlateOnPrimary = Color(0xFFFFFFFF)
+val BlueSlatePrimaryContainer = Color(0xFFBEEBFF)
+val BlueSlateOnPrimaryContainer = Color(0xFF001F28)
+val BlueSlateSecondary = Color(0xFF4E6268)
+val BlueSlateOnSecondary = Color(0xFFFFFFFF)
+val BlueSlateBackground = Color(0xFFFBFCFD)
+val BlueSlateOnBackground = Color(0xFF191C1D)
+val BlueSlateSurface = Color(0xFFFBFCFD)
+val BlueSlateOnSurface = Color(0xFF191C1D)
+val BlueSlateSurfaceVariant = Color(0xFFDCE4E8)
+val BlueSlateOnSurfaceVariant = Color(0xFF40484C)
+
+// Charcoal Blue Palette - Dark
+val CharcoalBluePrimary = BlueSlatePrimary // Color(0xFF424B54)
+val CharcoalBlueOnPrimary = BlueSlateOnPrimary // Color(0xFFFFFFFF)
+val CharcoalBluePrimaryContainer = BlueSlatePrimaryContainer // Color(0xFF2C353D)
+val CharcoalBlueOnPrimaryContainer = BlueSlateOnPrimaryContainer // Color(0xFFD1E5F0)
+val CharcoalBlueSecondary = BlueSlateSecondary // Color(0xFF94A9B4)
+val CharcoalBlueOnSecondary = BlueSlateOnSecondary // Color(0xFF191C1D)
+val CharcoalBlueBackground = Color(0xFF191C1D)
+val CharcoalBlueOnBackground = Color(0xFFE1E3E4)
+val CharcoalBlueSurface = Color(0xFF191C1D)
+val CharcoalBlueOnSurface = Color(0xFFE1E3E4)
+val CharcoalBlueSurfaceVariant = Color(0xFF40484C)
+val CharcoalBlueOnSurfaceVariant = Color(0xFFBFC8CC)
+
+val AppError = Color(0xFFBA1A1A)
+val AppOnError = Color(0xFFFFFFFF)
---
+++ app/src/main/java/app/ammar/watefinder/ui/theme/Theme.kt
@@ -0,0 +1,86 @@
+package app.ammar.watefinder.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+
+private val LightColorScheme = lightColorScheme(
+    primary = BlueSlatePrimary,
+    onPrimary = BlueSlateOnPrimary,
+    primaryContainer = BlueSlatePrimaryContainer,
+    onPrimaryContainer = BlueSlateOnPrimaryContainer,
+    secondary = BlueSlateSecondary,
+    onSecondary = BlueSlateOnSecondary,
+    background = BlueSlateBackground,
+    onBackground = BlueSlateOnBackground,
+    surface = BlueSlateSurface,
+    onSurface = BlueSlateOnSurface,
+    surfaceVariant = BlueSlateSurfaceVariant,
+    onSurfaceVariant = BlueSlateOnSurfaceVariant,
+    error = AppError,
+    onError = AppOnError,
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = CharcoalBluePrimary,
+    onPrimary = CharcoalBlueOnPrimary,
+    primaryContainer = CharcoalBluePrimaryContainer,
+    onPrimaryContainer = CharcoalBlueOnPrimaryContainer,
+    secondary = CharcoalBlueSecondary,
+    onSecondary = CharcoalBlueOnSecondary,
+    background = CharcoalBlueBackground,
+    onBackground = CharcoalBlueOnBackground,
+    surface = CharcoalBlueSurface,
+    onSurface = CharcoalBlueOnSurface,
+    surfaceVariant = CharcoalBlueSurfaceVariant,
+    onSurfaceVariant = CharcoalBlueOnSurfaceVariant,
+    error = AppError,
+    onError = AppOnError,
+)
+
+
+@Composable
+fun WaTeFinderTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content,
+    )
+}
