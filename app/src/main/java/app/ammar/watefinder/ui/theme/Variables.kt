7b382f5fb1ea91eec5a68779a5dd52a8b564796f
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2026-08-04 16:54:55 +0300

    add theme variables

---
+++ app/src/main/java/app/ammar/watefinder/ui/theme/Variables.kt
@@ -0,0 +1,20 @@
+package app.ammar.watefinder.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+
+object Variables {
+    val Space100 = 4.dp
+    val Space200 = 8.dp
+    val Space300 = 12.dp
+    val Space400 = 16.dp
+    val Space500 = 20.dp
+    val Space600 = 24.dp
+
+    val Radius100 = 4.dp
+    val Radius200 = 8.dp
+    val Radius300 = 12.dp
+    val Radius400 = 16.dp
+    val Radius500 = 20.dp
+    val Radius600 = 24.dp
+}
